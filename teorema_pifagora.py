8c28f46104fef4a01d2122f6b831aac5ddfa0340
ivanklym <ivan.klymkovych1992@gmail.com>
2026-01-04 18:18:43 +0200

    first commit

---
+++ teorema_pifagora.py
@@ -0,0 +1,19 @@
+# a=int(input("Enter first catet")) 
+# b=int(input("Enter second catet")) 
+# c= (((a**2)+(b**2))**0.5) 
+# print(c) 
+name='ivan Klymkovych'
+# print(name[0])
+# print(name[1])
+# print(name[-1])
+print(len(name))
+print(name[0:6])
+print(name[::2])
+print(name[::3])
+print(name[::-1])
+print(name.upper())
+print(name.lower())
+print(name.replace('I','O'))
+print(name.capitalize())
+# 1
+#2
